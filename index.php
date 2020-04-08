8da91f72eb542ff807f77a4a615027068ea0d5d9
Jagseer singh <52886945+jp-js@users.noreply.github.com>
2020-04-08 20:04:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<link href="css/reset.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="reset.css" rel="stylesheet" type="text/css">
+<link href="style.css" rel="stylesheet" type="text/css">
 <title>Comment Box</title>
 </head>
 <body style="background-color: white">
